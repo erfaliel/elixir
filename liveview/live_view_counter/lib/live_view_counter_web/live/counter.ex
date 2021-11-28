19dfde1d70570f8ce8539ddf910465d8b718298e
erfaliel <33790975+erfaliel@users.noreply.github.com>
2021-11-28 18:04:15 +0100

    Update counter.ex
    update accordingly with GenServer from live_vies_counter/counter.ex

--- liveview/live_view_counter/lib/live_view_counter_web/live/counter.ex
+++ liveview/live_view_counter/lib/live_view_counter_web/live/counter.ex
@@ -1,27 +1,26 @@
 defmodule LiveViewCounterWeb.Counter do
   use Phoenix.LiveView
+  alias LiveViewCounter.Count
+  alias Phoenix.PubSub
 
-  @topic "live"
+  @topic Count.topic
 
   def mount(_params, _session, socket) do
-    LiveViewCounterWeb.Endpoint.subscribe(@topic) # subscribe to the channel
-    {:ok, assign(socket, :val, 0)}
+    PubSub.subscribe(LiveViewCounter.Pubsub, @topic) # subscribe to the channel
+    
+    {:ok, assign(socket, :val, count.current())}
   end
 
-  def handle_event("inc", _value, socket) do
-    new_state = update(socket, :val, &(&1 + 1))
-    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
-    {:noreply, new_state}
+  def handle_event("inc", _, socket) do
+    {:noreply, assign(socket, :val, count.incr()}
   end
 
   def handle_event("dec", _value, socket) do
-    new_state = update(socket, :val, &(&1 - 1))
-    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
-    {:noreply, new_state}
+    {:noreply, ssign(socket, :val, count.decr()}
   end
 
-  def handle_info(msg, socket) do
-    {:noreply, assign(socket, val: msg.payload.val)}
+  def handle_info({:count, count}, socket) do
+    {:noreply, assign(socket, val: count)}
   end
 
   def render(assigns) do
